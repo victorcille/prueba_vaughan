9db5b36198c10328c23ad5b10463378daa88d76c
victorcille <cgutierrezvictor@gmail.com>
2024-12-03 17:25:55 +0100

    Call to tiny url to short url send in resquest body and resturned in response

--- app/Http/Controllers/Api/AcortadorUrlsController.php
+++ app/Http/Controllers/Api/AcortadorUrlsController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class AcortadorUrlsController extends Controller
 {
+    const TINY_URL = "https://tinyurl.com/api-create.php";
+
     public function acortadorUrl(Request $request): JsonResponse
     {
         $validator = Validator::make($request->all(), [
@@ -22,8 +26,19 @@ class AcortadorUrlsController extends Controller
             ], Response::HTTP_BAD_REQUEST);
         }
 
+        try {
+            $tinyUrl = self::TINY_URL . "?url={$request->input('url')}";
+            $response = Http::get($tinyUrl);
+        } catch (Exception $e) {
+            report($e); // Guardamos el log con el error en nuestro fichero de logs
+
+            return response()->json([
+                'error' => "Something went wrong while trying to short the url"
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
         return response()->json([
-            'message' => 'Hello World'
+            'url' => $response->body()
         ], Response::HTTP_OK);
     }
 }
