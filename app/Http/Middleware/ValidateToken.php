4b200cf06c2d5436bf51c7785eac506753e86da6
victorcille <cgutierrezvictor@gmail.com>
2024-12-03 17:51:39 +0100

    ValidateToken middleware creation (first approach) and bootstrap/app.php configuration to handle incoming request

---
+++ app/Http/Middleware/ValidateToken.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidateToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        dd('Pasa por el middleware');
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ValidateToken;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->use([ 'auth.bearer' => ValidateToken::class ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
